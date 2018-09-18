4be1ae349007c0a35c2fa4de8d0f867670e1feae
MelchorPech <15390232@itchetumal.edu.mx>
2018-09-17 19:30:37 -0500

    busqueda de vocal y reemplazo de acentos

--- app/src/main/java/mx/edu/itchetumal/dadm/generarrfc/MainActivity.java
+++ app/src/main/java/mx/edu/itchetumal/dadm/generarrfc/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,28 +13,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public String adecuarCadena(String cadena){
+        String cadAux= cadena.toUpperCas().trim();
+
+        char[] vocalesAcentuadas = {'Á', 'É', 'Í', 'Ó', 'Ú'};
+        char[] vocales = {'A', 'E', 'I', 'O', 'U',};
+        for (byte pos = 0; pos <= vocalesAcentuadas.length; pos++) {
+            apPaterno.replace(vocalesAcentuadas[pos], vocales[pos]);
+        }
+        return cadAux;
+    }
 
     public void GenerarRFC(View v){
-        //Tomar la primer letra y primer vocal del apellido paterno
+        // Tomar la primer letra y primer vocal del apellido paterno
         EditText editTextApPa = (EditText) findViewById(R.id.editTextApPa);
-        editTextApPa.getText().CharAt(0);
 
+        //Hacer método que reciba cadeda y devuelva en mayúsculas
+        // sin espacio al inicio o fin sin acentos
+        String apPaterno = adecuarCadena(editTextApPa.getText().toString());
+        Toast.makeText(this,"cadena convertida",Toast.LENGTH_LONG).show();
+        char PrimerLetraAP = apPaterno.CharAt(0);
 
-        char[] vocales ={'A','E','I','O','U','a','e','i','o','u'};
-        String CadAux="Pedro Perez";
-        char primeraLetra = CadAux.charAt(0);
+        char primerVocal=" ";
+        byte pos =0;
+        boolean encontrado =false;
+        for (byte pos = 0; pos <= vocalesAcentuadas.length; pos++) {
+            if (apPaterno.indexOf(vocales[pos])>=0){
+                encontrada =true;
+                break;
+            }
+        }
 
-
-        Char letraAp = new Character(CadAux.charAt(0));
+        //String CadAux="Pedro Perez";
+        //char primeraLetra = CadAux.charAt(0);
+        //Char letraAp = new Character(CadAux.charAt(0));
 
 
         //Tomar la primer letra del apellido materno
-        EditText editTextApMa = (EditText) findViewById(R.id.editTextApMa);
-        editTextApPa.getText().CharAt(0);
 
         //Tomar la primer letra del primer nombre
-        EditText editTextNombre = (EditText) findViewById(R.id.editTextNombre);
-        editTextApPa.getText().CharAt(0);
 
         //Tomar los ùltimo 2 digitos del año de nacimiento
 

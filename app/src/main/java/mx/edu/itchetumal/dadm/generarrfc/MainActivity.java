de20d1f7fb6d5414ebf5ac2655471efe148f0d61
MelchorPech <15390232@itchetumal.edu.mx>
2018-09-21 14:45:28 -0500

    metodos aleatorio

--- app/src/main/java/mx/edu/itchetumal/dadm/generarrfc/MainActivity.java
+++ app/src/main/java/mx/edu/itchetumal/dadm/generarrfc/MainActivity.java
@@ -83,7 +83,12 @@ public class MainActivity extends AppCompatActivity {
         //Tomar los 2 difitos del dia de nacimiento
 
         //Generar 3 caracteres(Letras mayusculas o nùmeros)
-
+        char[] elementos={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','E','S','T','U','V','W','X','Y','Z'};
+        char [] aleatorio= new char[3];
+        for(int i=0;i<=2;i++){
+            aleatorio[i]=elementos[(int)(Math.random()*36)];
+        }
         //Concatenar lo anterior y mostrar el RFC
+
     }
 }
